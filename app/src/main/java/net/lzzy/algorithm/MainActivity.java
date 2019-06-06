4bf1c469eef4a82c76856cbdae15c23e20670f06
retry <gexiangyou@gmail.com>
2019-06-06 15:36:22 +0800

    Initial commit

---
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -0,0 +1,66 @@
+package net.lzzy.algorithm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Random;
+
+/**
+ * @author Administrator
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Integer[] items;
+    private EditText edtItems;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtItems = findViewById(R.id.activity_main_edt_items);
+        findViewById(R.id.activity_main_btn_generate).setOnClickListener(this);
+        findViewById(R.id.activity_main_btn_sort).setOnClickListener(this);
+        tvResult = findViewById(R.id.activity_main_tv_result);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.activity_main_btn_generate:
+                generateItems();
+                displayItems(edtItems);
+                break;
+            case R.id.activity_main_btn_sort:
+                directSort();
+                displayItems(tvResult);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void displayItems(TextView tv) {
+        String display = "";
+        for (Integer i : items) {
+            display = display.concat(i + ",");
+        }
+        display = display.substring(0, display.length() - 1);
+        tv.setText(display);
+    }
+
+    private void directSort() {
+        //todo:直接选择排序的具体实现
+    }
+
+    private void generateItems() {
+        items = new Integer[10];
+        Random generator = new Random();
+        for (int i = 0; i < items.length; i++) {
+            items[i] = generator.nextInt(99);
+        }
+    }
+}
