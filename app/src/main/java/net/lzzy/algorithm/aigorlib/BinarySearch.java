1a85f4649a1046b21bad6cda3cccd90cf2db5f66
Administrator <1521309868@qq.com>
2019-06-27 15:08:25 +0800

    二分查找

--- app/src/main/java/net/lzzy/algorithm/aigorlib/BinarySearch.java
+++ app/src/main/java/net/lzzy/algorithm/aigorlib/BinarySearch.java
@@ -12,6 +12,23 @@ public class BinarySearch <T extends Comparable<? super T>> extends BaseSearch<T
 
     @Override
     public int search(T key) {
+        long start=System.currentTimeMillis();
+        int left=0;
+        int right=items.length-1;
+        while (left<=right){
+            int mid=(left+right)/2;
+            int comp=compare(key,items[mid]);
+            if (comp==0){
+                setDuration(System.currentTimeMillis()-start);
+                return mid;
+            }else{
+                if (comp>0){
+                    left=mid-1;
+                }
+            }
+            setDuration(System.currentTimeMillis()-start);
+            return -1;
+        }
         return 0;
     }
 }
